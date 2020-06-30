6826b87813fa6e363e4a45c29336090c84fa592c
snipa <snipa@gmail.com>
2020-06-30 14:51:50 -0500

    Added banner and updated logi

--- password_generator.py
+++ password_generator.py
@@ -6,64 +6,77 @@ import pyperclip
 import sys
 
 """
-    @author: Emmanuel Hernandez - @snipa.v1
+@author: Emmanuel Hernandez - @snipa.v1
 
     This script will generate a complex password offline through terminal and
     copy the password to clipboard
 
 """
+BANNER = """
+-------------------------------------------------------
+______ _______ __   _  _____  _______ _______ _______
+|  ____ |______ | \  | |_____] |_____| |______ |______
+|_____| |______ |  \_| |       |     | ______| ______|
+
+-------------------------------------------------------
+"""
+print(BANNER)
+
+SPECIAL_CHARS = "!@#$%^&*()"
+
+
+class Color:
+    OKBLUE = '\033[94m'
+    ENDC = '\033[0m'
+
+
+def generate_password(length):
+    return "".join(
+        random.
+        choice(string.ascii_lowercase + string.digits + SPECIAL_CHARS) for
+        _ in range(length))
 
 
 def main():
-    special_chars = "!@#$%^&*()"
     word_length = 0
-
     is_long = False
     while not is_long:
         length = int(
             input("[.] What length of password would you like: \
                   \n    (minimum = 12) \n\n> "))
         if length >= 12:
-            if is_long:
-                word_length = length
+            word_length = length
+            is_long = True
             break
         else:
             print("\n[!] Password must be at least 12 characters long!\n")
 
-    def generate_password(line):
-        return "".join(
-            random.
-            choice(string.ascii_lowercase + string.digits + special_chars) for
-            _ in range(line))
-
-    done = False
     yes_no = ""
+    print("[.] Word length is: {}".format(str(word_length)))
 
     print("\n[*] Press 'e' or type 'exit' to exit")
 
-    while not done or yes_no != 'e':
+    while yes_no != 'e':
 
         password = generate_password(word_length)
 
         yes_no = input(
-            "\n[.] Would you like to use: '{}' (y or n)?\n\n> ".
-            format(password))
+            f"\n[.] Would you like to use: '{Color.OKBLUE}{password}{Color.ENDC}' (y or n)?\n\n> ")
 
         yes_no = yes_no.lower()
 
         if yes_no == "e" or yes_no == 'exit':
             print("\n[%] Exiting...")
             sys.exit()
-        if yes_no == 'y' or yes_no == 'yes':
-            pyperclip.copy(password)
-            print("\n[%] '{}' copied to clipboard successfully!\n".
-                  format(password))
-            done = True
-            break
-        elif yes_no == 'n' or yes_no == 'no':
-            continue
-        else:
-            print("\n[!] Please enter: 'y' or 'n'")
+            if yes_no == 'y' or yes_no == 'yes':
+                pyperclip.copy(password)
+                print(
+                    f"\n[%] {Color.OKBLUE}'{password}'{Color.ENDC} copied to clipboard successfully!\n")
+                sys.exit()
+            elif yes_no == 'n' or yes_no == 'no':
+                continue
+            else:
+                print("\n[!] Please enter: 'y' or 'n'")
 
 
 if __name__ == '__main__':
